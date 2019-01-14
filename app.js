646454c443582e9265c3c419d6c332226675055f
Varsha <manisha@manisha-All-Series>
2019-01-14 19:25:21 +0530

    commit

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ let app = express();
 
 app.use('/',routes);
 
-app.use((err, req, res, next) => {
-  res.status(500).send({
-    "Error": err.stack
-  });
-});
-
 app.set('port', process.env.PORT || 4000 );
 app.listen(app.get('port') , function(){
 console.log('Server started on port : '+ app.get('port'));
